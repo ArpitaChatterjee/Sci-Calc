d7322bcdb501f6834a4a0503635df91b914fe941
ArpitaChatterjee <48859001+ArpitaChatterjee@users.noreply.github.com>
2020-09-10 14:08:31 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,4 @@
-/* ------------------------------------------------- */
-/*                By Code Explained                  */
-/*          www.youtube.com/Code Explained           */
-/* ------------------------------------------------- */
 
-// CALCULATOR BUTTONS
 let calculator_buttons = [
     {
         name : "rad",
@@ -218,7 +213,7 @@ let calculator_buttons = [
 
 // GAMMA FUNCTINON
 function gamma(n) {  // accurate to about 15 decimal places
-    //some magic constants 
+    
     var g = 7, // g represents the precision desired, p is the values of p[i] to plug into Lanczos' formula
         p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
     if(n < 0.5) {
